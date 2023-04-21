d281cc7d66148c5968ecf08e48e70383b8752c8b
eshaanmandal <eshaanmandal29@gmail.com>
2023-04-21 23:51:41 +0530

    Updated the method for evaluation on validation set as well

--- inference.py
+++ inference.py
@@ -11,6 +11,8 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 num_feats = 64
 path_to_anomaly = '../Anomaly'
 path_to_normal = '../Normal'
+path_to_val = '..'
+
 checkpoint = torch.load('./checkpoints/best_ssl_bce.pth')
 model = CLIPFormer(num_layers=1, nhead=4).to(device)
 model.load_state_dict(checkpoint['model_state_dict'])
@@ -18,44 +20,75 @@ model.load_state_dict(checkpoint['model_state_dict'])
 train_anomaly_ds = AnomalyVideo(path_to_anomaly, num_feats)
 train_normal_ds = NormalVideo(path_to_normal, num_feats)
 
-train_a_dl = DataLoader(train_anomaly_ds, batch_size=32, num_workers=16)
-train_n_dl = DataLoader(train_normal_ds, batch_size=32, num_workers=16)
+train_a_dl = DataLoader(train_anomaly_ds, batch_size=32, num_workers=16, shuffle=True)
+train_n_dl = DataLoader(train_normal_ds, batch_size=32, num_workers=16, shuffle=True)
 
+
+# validation data
+valid_ds = ValidationVideo(path_to_val, num_feats)
+val_dl = DataLoader(valid_ds, batch_size=valid_bs, shuffle=True, num_workers=16)
 
 # performing some inference
-def infer():
+def infer(data):
     model.eval()
 
-    print('Performing evaluation on the model')
-    # gts = []
-    preds = []
-    with torch.no_grad():
-        for anomaly, normal in tqdm(zip(train_a_dl, train_n_dl), total=min(len(train_a_dl), len(train_n_dl))):
-            clip_a, _ = anomaly
-            clip_n, _ = normal
+    if data == 'train':
+        print('Performing evaluation on the model')
+        # gts = []
+        preds = []
+        with torch.no_grad():
+            for anomaly, normal in tqdm(zip(train_a_dl, train_n_dl), total=min(len(train_a_dl), len(train_n_dl))):
+                clip_a, _ = anomaly
+                clip_n, _ = normal
 
-            # gt_a = [1 for _ in clip_a.shape[0]]
-            # gt_n = [0 for _ in clip_n.shape[1]]
+                # gt_a = [1 for _ in clip_a.shape[0]]
+                # gt_n = [0 for _ in clip_n.shape[1]]
 
-            # pass data to device
-            clip_a, clip_n = clip_a.to(device), clip_n.to(device)
+                # pass data to device
+                clip_a, clip_n = clip_a.to(device), clip_n.to(device)
 
-            score_a, score_n = model(clip_a), model(clip_n)
+                score_a, score_n = model(clip_a), model(clip_n)
 
-            score_a, score_n = score_a.squeeze(-1), score_n.squeeze(-1)
+                score_a, score_n = score_a.squeeze(-1), score_n.squeeze(-1)
 
-            pred_a, pred_n = torch.max(score_a, dim=1)[0], torch.max(score_n, dim=1)[0]
+                pred_a, pred_n = torch.max(score_a, dim=1)[0], torch.max(score_n, dim=1)[0]
 
-            combined_pred = torch.cat((pred_a, pred_n))
+                combined_pred = torch.cat((pred_a, pred_n))
 
-            combined_pred = combined_pred.cpu().detach().numpy()
+                combined_pred = combined_pred.cpu().detach().numpy()
 
-            preds.extend(combined_pred)
+                preds.extend(combined_pred)
 
-    return preds
+        return preds
+
+    elif data == 'valid':
+        preds = []
+        with torch.no_grad():
+            for clip_fts, _, _ in tqdm(val_dl):
+                clip_fts = clip_fts.to(device)
+
+                scores = model(clip_fts)
+
+                scores = scores.squeeze(-1)
+
+                pred = torch.max(scores, dim=1)[0]
+
+                pred = pred.cpu().detach().numpy()
+
+                preds.extend(pred)
+
+        return preds
 
 
-preds = infer()
+
+
+
+
+
+
+
+
+preds = infer(data='valid')
 
 plt.figure()
 plt.hist(preds)
